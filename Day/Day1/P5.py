d1f9ba1a1de53f3f7abefdcca08d67c82d818d13
athishay <2025cs_athishayk_b@nie.ac.in>
2026-09-21 14:49:26 +0530

    Decorator named my_decorator is created and and invoked

---
+++ Day/Day1/P5.py
@@ -0,0 +1,13 @@
+#Decorator
+def my_decorator(func):
+    def wrapper():
+        print("Before")
+        func()
+        print("After")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello !")
+
+say_hello()
