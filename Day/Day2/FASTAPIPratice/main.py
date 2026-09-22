ad42183a9ea196afee3d8797195eb85e6ae638f5
athishay <2025cs_athishayk_b@nie.ac.in>
2026-09-22 15:19:27 +0530

    Pydantic Model

--- Day/Day2/FASTAPIPratice/main.py
+++ Day/Day2/FASTAPIPratice/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 @app.get("/")
 def home():
@@ -26,3 +27,13 @@ def get_result(usn):
 @app.get("/candidate/{rollno}")
 def get_result(rollno: int):
     return  {"Result": "Distinction","rollno":rollno,"type":str(type(rollno))}
+
+#Pydantic Model
+class Item(BaseModel):
+    name:str
+    price:float
+    in_stock: bool = True
+
+@app.post("/items")
+def create_item(item:Item):
+    return {"received": item,"total_price":item.price*1.18}
