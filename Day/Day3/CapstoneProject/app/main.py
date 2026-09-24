2db74d3fa1289d60fd2b0bfebfae5aec8c29d839
athishay <2025cs_athishayk_b@nie.ac.in>
2026-09-24 11:28:13 +0530

    CP: 4

--- Day/Day3/CapstoneProject/app/main.py
+++ Day/Day3/CapstoneProject/app/main.py
@@ -3,11 +3,14 @@ from fastapi import FastAPI
 from app.config import settings
 from app.database import ping_database
 from app.routers import users
+from app.routers import categories
+
 
 #Creating FastAPi app instance
 app = FastAPI(title=settings.APP_NAME)
 
 app.include_router(users.router)
+app.include_router(categories.router)
 
 #This function runs once when the server starts. It checks the DB connection
 @app.on_event("startup")
